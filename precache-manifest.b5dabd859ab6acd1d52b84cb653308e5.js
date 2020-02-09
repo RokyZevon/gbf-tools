05569bc8e8cc0dd5bbcc5d94b0ae6fe68bdd8947
niccolozy <zy_graysky@hotmail.com>
2020-02-09 22:02:39 +0000

    Deploying to gh-pages from  - e8e8718cfcf446e4e5b058d2deda6e9eb81507fe 🚀

---
+++ precache-manifest.b5dabd859ab6acd1d52b84cb653308e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd4a06df06eada508db0f52034332d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95356de1bff4d5991f34",
+    "url": "./static/js/2.f0dc165c.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.f0dc165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87805be2088e2af9f46",
+    "url": "./static/js/main.afd8635f.chunk.js"
+  },
+  {
+    "revision": "a1694d1303f019cd885a",
+    "url": "./static/js/runtime-main.da266498.js"
+  }
+]);
